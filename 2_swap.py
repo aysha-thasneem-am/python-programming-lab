b152bb8c3c3cf64f4b30d831b43d858e7288c42f
Aysha Thasneem AM <ayshathasneemam@gmail.com>
2022-10-21 10:29:04 +0530

    Add files via upload
    Python Program to swap 2 numbers without using a temporary variable.

---
+++ 2_swap.py
@@ -0,0 +1,4 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("values of a and b before swapping is:", a, b)
+print("values of a and b after swapping:", b,a)
