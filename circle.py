b86cdafb461b35b0658462253243c37cf896a694
Aysha Thasneem AM <ayshathasneemam@gmail.com>
2022-10-21 10:14:19 +0530

    Add files via upload
    Python Program to find the area and perimeter of a circle.

---
+++ circle.py
@@ -0,0 +1,6 @@
+PI = 3.14
+R = float(input("Enter radius of the circle: "))
+area = (PI*R*R)
+perimeter = (2*PI*R)
+print("The area of circle is", area)
+print("The perimeter of circle is", perimeter)
