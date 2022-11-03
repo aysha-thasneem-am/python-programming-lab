cfbbfb08883eaf577fba8cf550327eb242e4332d
Aysha Thasneem AM <ayshathasneemam@gmail.com>
2022-11-03 11:57:03 +0530

    Add files via upload

---
+++ 14_greatestno.py
@@ -0,0 +1,15 @@
+a=int(input("Enter first no.: "))
+b=int(input("Enter 2nd no.: "))
+c=int(input("Enter 3rd no.: "))
+"""if a>b:
+    if a>c:
+        print(a, " is greatest")
+    else:
+        print(c, "is greatest")
+elif b>c:
+    print(b, " is greatest")
+else:
+    print(c, " is greatest")
+
+"""
+print(max(a,b,c))
