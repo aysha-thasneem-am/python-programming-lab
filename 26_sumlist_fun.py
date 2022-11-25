72f43bc93ef6f34b9b5a3751ae45688f74c7a46c
Aysha Thasneem AM <ayshathasneemam@gmail.com>
2022-11-25 09:28:53 +0530

    Update 26_sumlist_fun.py

--- 26_sumlist_fun.py
+++ 26_sumlist_fun.py
@@ -1,11 +1,8 @@
-def sumlist(l,size):
-    if size == 0:
-        return 0
-    else:
-        return l[size-1]+sumlist(l,size-1)
-a=int(input("Enter list elements: "))
-"""a=a.split(" ")
-list1=list(map(int,a))"""
-total=sumlist(list1,len(list1))
-
-print("Sum of the list elements: ",total)
+s = 0
+def sum1(a):
+    global s
+    s+=a
+    return s
+li =list(map(int,input("Enter a numbers ").split()))
+s=list(map(sum1,li))
+print(s[-1])
