f17107cc68d557aac321a457099b760019e5b3e6
Aysha Thasneem AM <ayshathasneemam@gmail.com>
2022-11-25 10:06:55 +0530

    Add files via upload
    Construct the following pattern:
    *
    **
    ***
    ****
    *****
    ****
    ***
    **
    *

---
+++ 32_pattern_fun.py
@@ -0,0 +1,9 @@
+rows = 5
+for i in range(0, rows):
+    for j in range(0, i + 1):
+        print("*", end=' ')
+    print(" ") 
+for i in range(rows + 1, 0, -1):
+    for j in range(1, i - 1):  
+        print("*", end=' ')  
+    print(" ")
